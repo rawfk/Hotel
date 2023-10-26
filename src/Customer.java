e1b788281156499bbce88256e67f2cfffc3b1c05
Jeon <jeongsh950518@gmail.com>
2023-10-26 16:51:17 +0900

    수정1

--- src/Customer.java
+++ src/Customer.java
@@ -2,20 +2,49 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-/**
+/*
  * 고객 - 이름, 전화번호, 소지금
  */
 public class Customer {
     private String name;
     private String phone;
-    private long money;
+    private int cash;
     private List<UUID> reservationNumberList;
+    private final String id;
+    private final String pw;
 
-    public Customer(String name, String phone, long money) {
+
+    public Customer(String name, String phone, int cash, String id, String pw) {
         this.name = name;
         this.phone = phone;
-        this.money = money;
+        this.cash = cash;
+        this.id = id;
+        this.pw = pw;
         this.reservationNumberList = new ArrayList<>();
     }
 
+
+    public String getName(){
+        return name;
+    }
+
+    public String getPhone(){
+        return phone;
+    }
+
+    public int getCash(){
+        return cash;
+    }
+
+    public void setCash(int cash){
+        this.cash = cash;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public String getPw(){
+        return pw;
+    }
 }
--- src/Login.java
+++ src/Login.java
@@ -1,6 +1,29 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Login {
-    private List<Customer> customerList;
+    private static List customerList;
+    private Boolean login;      //
 
+    public Login(){
+    }
+    public Login(List<Customer> customerList){
+        this.customerList=customerList;
+    }
+    public Login(Boolean login){
+        this.login=login;
+    }
+
+
+
+    public List<Customer> getCustomerList(){
+        return customerList;
+    }
+    public Boolean getLogin(){
+        return login;
+    }
+
+    public void setLogin(Boolean login) {
+        this.login = login;
+    }
 }
